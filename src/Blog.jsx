f65e0d92199ce13228edcf25f17d86147cac1ffa
seiya_t <kenose0328@gmail.com>
2021-07-03 22:50:44 +0900

    stateの設定、取得、変更の練習

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -4,16 +4,26 @@ import Article from './Article';
 class Blog extends React.Component {
   constructor(props) {
     super(props); // これを記述するとクラス内でthis.propsを使える
+    this.state = {
+      isPublished: false,
+      order: 0
+    }
   }
+
+  // 公開状態を反転させる関数を定義する
+  togglePublished = () => {
+    this.setState({
+      isPublished: !this.state.isPublished
+    })
+  };
+
   render() {
-    const authorName = 'Torahack'
     return(
       <>
         <Article
           title={'Reactの使い方'}
-          order = {3}
-          isPublished={true}
-          author={authorName}_
+          isPublished={this.state.isPublished}
+          toggle={() => this.togglePublished()}
         />
       </>
     )
