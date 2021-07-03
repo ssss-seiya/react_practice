c1c2e6e914b9293f52dcc4feed5896a6e26877aa
seiya_t <kenose0328@gmail.com>
2021-07-04 00:04:19 +0900

    stateの設定、取得、変更の練習

--- src/Article.jsx
+++ src/Article.jsx
@@ -2,20 +2,18 @@ import React from 'react';
 
 const Article = (props) => {
 
-  let PublishState = '';
-  if (props.isPublished) {
-    PublishState = '公開'
-  } else {
-    PublishState = '非公開'
-  }
   return (
     <div>
       <h2>{props.title}</h2>
-      <p>順番は{props.order}です</p>
-      <p>著書：{props.author}</p>
-      <p>{PublishState}</p>
+      <label htmlFor='check'>公開状態:</label>
+      <input
+        type='checkbox'
+        checked={props.isPublished}
+        id='check'
+        onClick={() => props.toggle()}
+      />
     </div>
   )
 };
 
-export default Article
+export default Article;
